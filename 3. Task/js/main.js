1ed496da7e38ddaf7368834f9ebecde40f81cbc9
rick-77-2024 <rick_77@ukr.net>
2025-06-10 11:45:27 +0300

    Add files via upload

---
+++ 3. Task/js/main.js
@@ -0,0 +1,180 @@
+console.log('---Массив из 10 элементов---');
+
+let arr = [
+    [10],
+    [true],
+    ['text'],
+    [3.14],
+    [null],
+    [undefined],
+    [5465486484564654894654684698456],
+    [
+        [5],
+        [10]
+    ],
+    [
+        [true],
+        [false]
+    ],
+    ['final']
+];
+
+console.log(arr);
+
+console.log('---Три объекта описываэщие книгу---');
+
+let book = {
+    title: 'TITLE',
+    pageCount: 200,
+    genre: 'PROGRAMMING'
+}
+
+console.log(book);
+
+console.log('---Три объекта и массив авторов---');
+
+let book1 = {
+    title: 'TITLE',
+    pageCount: 200,
+    genre: 'PROGRAMMING',
+    authors: [
+        author1 =
+            [
+                'name',
+                'age'
+            ],
+        author2 =
+            [
+                'name',
+                'age'
+            ],
+        author3 =
+            [
+                'name',
+                'age'
+            ]
+    ]
+}
+
+console.log(book1);
+
+console.log('---Десять объектов и пароли пользователей---');
+
+let users = [
+    {
+            name: 'User1',
+            username: 'UserName1',
+            password: 'password1'
+    },
+    {
+            name: 'User2',
+            username: 'UserName2',
+            password: 'password2'
+    },
+    {
+            name: 'User3',
+            username: 'UserName3',
+            password: 'password3'
+    },
+    {
+            name: 'User4',
+            username: 'UserName4',
+            password: 'password4'
+    },
+    {
+            name: 'User5',
+            username: 'UserName5',
+            password: 'password5'
+    },
+    {
+            name: 'User6',
+            username: 'UserName6',
+            password: 'password6'
+    },
+    {
+            name: 'User7',
+            username: 'UserName7',
+            password: 'password7'
+    },
+    {
+            name: 'User8',
+            username: 'UserName8',
+            password: 'password8'
+    },
+    {
+            name: 'User9',
+            username: 'UserName9',
+            password: 'password9'
+    },
+    {
+            name: 'User10',
+            username: 'UserName10',
+            password: 'password10'
+    }
+]
+
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
+
+console.log('---Сохранять температуру среды---');
+let temperature = [
+    {
+        day: 'Monday',
+        morning: 10,
+        noon: 20,
+        evening: 15
+    },
+    {
+        day: 'Tuesday',
+        morning: 10,
+        noon: 20,
+        evening: 15
+    },
+    {
+        day: 'Wednesday',
+        morning: 10,
+        noon: 20,
+        evening: 15
+    },
+    {
+        day: 'Thirday',
+        morning: 10,
+        noon: 20,
+        evening: 15
+    },
+    {
+        day: 'Friday',
+        morning: 10,
+        noon: 20,
+        evening: 15
+    },
+    {
+        day: 'Saturday',
+        morning: 10,
+        noon: 20,
+        evening: 15
+    },
+    {
+        day: 'Sunday',
+        morning: 10,
+        noon: 20,
+        evening: 15
+    },
+
+]
+
+console.log(temperature);
+
+
+
+
+
+
