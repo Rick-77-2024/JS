025dd6292fa9f210ee4c81d7faae7c027d8b26c0
rick-77-2024 <rick_77@ukr.net>
2025-06-10 15:35:57 +0300

    Add files via upload

---
+++ hw1/hw1.1/js/main.js
@@ -0,0 +1,45 @@
+console.log('---First task---');
+let hello = 'hello';
+let owu = 'owu';
+let ua = 'ua';
+let one = 1;
+let ten = 10;
+let ottr = 123;
+let PI = 3.14;
+let num2 = 2.7;
+let tr = true;
+let fs = false;
+
+console.log(hello); console.log(owu);
+console.log(ua); console.log(one);
+console.log(ten); console.log(ottr);
+console.log(PI); console.log(num2);
+console.log(tr); console.log(fs);
+
+console.log('---Second task---');
+const firstName = 'Vitaliy'
+const middleName = 'Michailovich';
+const lastName = 'Turik';
+let person = firstName + ' ' + middleName + ' ' +lastName
+let person2 = `${firstName} ${middleName} ${lastName}`
+
+console.log(person); console.log(person2);
+
+console.log('---Third task---');
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a, a);
+console.log(typeof b, b);
+console.log(typeof c, c);
+
+
+console.log('---Addition task prompt---');
+const name = prompt('Имя?', 'Имя')
+const surname = prompt('Фамилия?', 'Фамилия')
+const age = prompt('Возраст?', 'Возраст')
+
+
+
