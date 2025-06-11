5f6f0a1a20fa5d083b8315d7001a1765bd0a9cbb
rick-77-2024 <rick_77@ukr.net>
2025-06-11 16:45:48 +0300

    Update main.js

--- hw3/hw3.2/js/main.js
+++ hw3/hw3.2/js/main.js
@@ -1,7 +1,7 @@
 // #TYj7ncx
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
 
-for (let x = 1; x <= 10; x++) {
+for (let x = 0; x <= 10; x++) {
 
     document.write('<div>'+ x +'</div>');
 }
