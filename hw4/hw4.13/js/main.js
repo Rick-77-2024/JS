4dca639ad17488d8113327c44e22363f3ea4ae49
rick-77-2024 <rick_77@ukr.net>
2025-06-26 16:54:38 +0300

    Add files via upload

--- hw4/hw4.13/js/main.js
+++ hw4/hw4.13/js/main.js
@@ -2,6 +2,7 @@
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD') // => 400
 
-function exchange(sumUAH,currencyValues,exchangeCurrency) {
-    
+function exchange(sumUAH, currencyValues) {
+    return sumUAH / currencyValues;
 }
+console.log(exchange(10000, 25));
