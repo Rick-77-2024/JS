6b62edb6eca2618cc220b24c62660edbd08690be
rick-77-2024 <rick_77@ukr.net>
2025-06-27 15:17:11 +0300

    Add files via upload

---
+++ hw5/hw5.3/js/main.js
@@ -0,0 +1,9 @@
+// #Mbiz5K4yFe7
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+
+const SquareofCilinder = area => 2 * 3.14 * r * h + 2 * 3.14 * r * r;
+
+const r = 8;
+const h = 16;
+const area = SquareofCilinder(r, h);
+console.log('Square of cilinder: ' + area);
