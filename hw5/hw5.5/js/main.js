3d6a6cfd0909542c9c44b3971a031a850734ecf5
rick-77-2024 <rick_77@ukr.net>
2025-06-27 15:40:52 +0300

    Add files via upload

---
+++ hw5/hw5.5/js/main.js
@@ -0,0 +1,6 @@
+// #59g0IsA
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+
+const paragraph = (text) => document.write("<p>" + text + "</p>")
+
+paragraph("I show you how to write in paragraph");
