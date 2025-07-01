3086de88b9b681c46f75b371edfded3c6dd84685
rick-77-2024 <rick_77@ukr.net>
2025-07-01 16:50:31 +0300

    Add files via upload

---
+++ hw5/hw5.10/js/main.js
@@ -0,0 +1,17 @@
+// #pghbnSB
+// - створити функцію яка повертає найменьше число з масиву
+
+const minNumber = (arr) => {
+    let minNumber = arr[0];
+    for (let x = 1; x < arr.length; x++) {
+        if (arr[x] < minNumber) {
+            minNumber = arr[x];
+        }
+    }
+    return minNumber;
+}
+
+const numbers = [20, 4, -15, 8, 25];
+const smallNum = minNumber(numbers);
+console.log(smallNum);
+
