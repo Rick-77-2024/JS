b38a2a3deb72d78b5c9543bee7f592a712f24d6b
rick-77-2024 <rick_77@ukr.net>
2025-07-03 10:39:46 +0300

    Add files via upload

---
+++ hw6/hw6.3/js/main.js
@@ -0,0 +1,6 @@
+// #ClDsAm7xba7
+// - Перевести до нижнього регістру настипні стрінгові значення
+//      'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+
+
---
+++ hw6/hw6.7/js/main.js
@@ -0,0 +1,8 @@
+// #5hqyKTfmc
+// - створити функцію sortNums(array,direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+// let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+
+
+
