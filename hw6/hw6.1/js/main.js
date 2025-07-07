ac6994d9a56924620f2533d8c3ae96ccd901e290
rick-77-2024 <rick_77@ukr.net>
2025-07-07 12:37:10 +0300

    Add files via upload

--- hw6/hw6.1/js/main.js
+++ hw6/hw6.1/js/main.js
@@ -1,6 +1,14 @@
 // #dFeorS3m7u
-// - Знайти та вивести довижину настипних стрінгових значень
+// - Знайти та вивести довижину наступних стрінгових значень
 //    'hello world', 'lorem ipsum', 'javascript is cool');
 
-
+let str1 = "hello world";
+let str2 = "lorem ipsum";
+let str3 = "javascript is cool";
+let length1 = str1.length;
+let length2 = str2.length;
+let length3 = str3.length;
+console.log(length1);
+console.log(length2);
+console.log(length3);
 
