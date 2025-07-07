7aee2660e415c09f9ead7fe40ec092ac9f9edd01
rick-77-2024 <rick_77@ukr.net>
2025-07-07 16:29:14 +0300

    Add files via upload

--- hw6/hw6.6/js/main.js
+++ hw6/hw6.6/js/main.js
@@ -1,2 +1,9 @@
 // #Rbr5kEQ
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+
+const arr = [10,8,-7,55,987,-1011,0,1050,0];
+
+const strtoArr = arr.map(obj => JSON.stringify(obj));
+
+console.log(strtoArr);
+
