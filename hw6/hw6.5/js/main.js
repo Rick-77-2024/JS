2f349fd6b7347a8e53b54d1459841a8537253a73
rick-77-2024 <rick_77@ukr.net>
2025-07-07 16:19:43 +0300

    Add files via upload

--- hw6/hw6.5/js/main.js
+++ hw6/hw6.5/js/main.js
@@ -3,5 +3,10 @@
 //    let str = 'Ревуть воли як ясла повні';
 //    let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
 
+function stringToWords(str) {
+    return str.split(" ");
+}
 
-
+let str = "Ревуть воли як ясла повні";
+let strArr = stringToWords(str);
+console.log(strArr);
