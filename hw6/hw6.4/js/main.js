62589ec9aa691e1d65a9de355c8525c0ca26d563
rick-77-2024 <rick_77@ukr.net>
2025-07-09 17:25:52 +0300

    Update main.js

--- hw6/hw6.4/js/main.js
+++ hw6/hw6.4/js/main.js
@@ -2,5 +2,6 @@
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
 
 let str = ' dirty string   '
+const s = str.trim();
 
-console.log(str.replaceAll(' ', ''));
+console.log(s);
