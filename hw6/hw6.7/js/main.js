7d641c7b46aac0725360286389a77b6f088e718d
rick-77-2024 <rick_77@ukr.net>
2025-07-09 16:45:11 +0300

    Add files via upload

--- hw6/hw6.7/js/main.js
+++ hw6/hw6.7/js/main.js
@@ -4,5 +4,17 @@
 // sortNums(nums,'ascending') // [3,11,21]
 // sortNums(nums,'descending') // [21,11,3]
 
+function sortNums(nums, direction) {
+    if (direction === "descending") {
+        return nums.sort((a, b) => b - a);
+    } else {
+        return nums.sort((a, b) => a - b);
+    }
+}
 
+let nums = [11, 21, 3];
+let sortedDescending = sortNums(nums, "descending");
+let sortedAscending = sortNums(nums, "ascending");
 
+ console.log(sortNums (nums, 'ascending'));
+ console.log(sortNums (nums, 'descending'));
--- hw6/hw6.9/js/main.js
+++ hw6/hw6.9/js/main.js
@@ -1,16 +1,96 @@
- #bolvdlhP
- описати колоду карт (від 6 до туза без джокерів)
- - знайти піковий туз
- - всі шістки
- - всі червоні карти
- - всі буби
- - всі трефи від 9 та більше
+//  #bolvdlhP
+//  описати колоду карт (від 6 до туза без джокерів)
+//  - знайти піковий туз
+//  - всі шістки
+//  - всі червоні карти
+//  - всі буби
+//  - всі трефи від 9 та більше
+//
+// {
+//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+//     value: '', // '6'-'10', 'ace','jack','queen','king'
+//     color:'', // 'red','black'
+// }
 
-{
-    cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
-    value: '', // '6'-'10', 'ace','jack','queen','king'
-    color:'', // 'red','black'
-}
+let coursesArray = [
+ {
+  title: 'JavaScript Complex',
+  monthDuration: 5,
+  hourDuration: 909,
+  modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+ },
 
+ {
+  title: 'Java Complex',
+  monthDuration: 6,
+  hourDuration: 909,
+  modules: ['html',
+   'css',
+   'js',
+   'mysql',
+   'mongodb',
+   'angular',
+   'aws',
+   'docker',
+   'git',
+   'java core',
+   'java advanced']
+ },
 
+ {
+  title: 'Python Complex',
+  monthDuration: 6,
+  hourDuration: 909,
+  modules: ['html',
+   'css',
+   'js',
+   'mysql',
+   'mongodb',
+   'angular',
+   'aws',
+   'docker',
+   'python core',
+   'python advanced']
+ },
+
+ {
+  title: 'QA Complex',
+  monthDuration: 4,
+  hourDuration: 909,
+  modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+ },
+
+ {
+  title: 'FullStack',
+  monthDuration: 7,
+  hourDuration: 909,
+  modules: ['html',
+   'css',
+   'js',
+   'mysql',
+   'mongodb',
+   'react',
+   'angular',
+   'aws',
+   'docker',
+   'git',
+   'node.js',
+   'python',
+   'java']
+ },
+
+ {
+  title: 'Frontend',
+  monthDuration: 4,
+  hourDuration: 909,
+  modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+ }
+];
+console.log(coursesArray.filter (value => {
+ return value.modules.includes('sass')
+}));
+
+console.log(coursesArray.filter (value => {
+ return value.modules.includes('docker')
+}));
 
